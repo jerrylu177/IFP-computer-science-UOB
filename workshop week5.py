7d81e6f259bfdfdd8dfaeb42aa35d2ed523072fe
jerrylu177 <Lujiangyu2005@163.com>
2024-10-25 13:15:15 +0100

    Add files via upload

---
+++ workshop week5.py
@@ -0,0 +1,41 @@
+print("hello")
+print("world")
+print("!")
+
+print('hello world!')
+
+print("    /|\ ")
+print("   / | \ ")
+print("  /  |  \ ")
+print(" /   |   \ ")
+print("/____|____\ ")
+
+name = "Jerry"
+age = "19"
+gender = "male"
+print("my name:" + name)
+print("my age:" + age)
+print("my gender:" + gender)
+
+# importing the sys module to find out python version of the director(must)
+import sys
+print(sys.version)
+
+# python has a module named datetime to work with dates and time(must)
+import datetime
+current_time = datetime.datetime.now()
+print(current_time)
+
+# double vs single quotes
+print('she said \"I like double quotes\"')
+print("But he said 'I like single quotes'")
+# how to ues both
+# print("She said "I like double quotes" but he said 'I like single quotes'")
+print("She said \"I like double quotes\" But he said \'I like single quotes\'")
+print("She said \"I like double quotes\" \nBut he said 'I like single quotes'")
+
+# individual tasks:
+print("Hello World!")
+print("Welcome to my Python Programming!")
+print('Albert Einstein said "Imagination is more important than knowledge. Knoweledge is limited. Imagination encircles the world."')
+print("Tom said, \"I have always liked to hear \'I am the best' as often as possible\"")
